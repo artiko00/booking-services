9302beb4881ef29bad8c4c89d84bf7ac619d7d90
Jassen Castillo <artiko00@gmail.com>
2022-04-01 17:59:32 -0300

    feature:58

--- packages/api-gateway/src/drivers/http/adapters/index.js
+++ packages/api-gateway/src/drivers/http/adapters/index.js
@@ -5,11 +5,13 @@ const statusAdapters = require('./status/handlers');
 const adminPanelAdapters = require('./admin_panel/handlers');
 const notificationsAdapters = require('./notifications/handlers');
 const placesAdapters = require('./places/handlers')
+const bookingAdapters = require('./booking/handlers')
 
 module.exports = {
 	mathAdapters,
 	statusAdapters,
 	adminPanelAdapters,
     notificationsAdapters,
-    placesAdapters
+    placesAdapters,
+	bookingAdapters
 }
